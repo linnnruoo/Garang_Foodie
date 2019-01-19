d07a5904a731eb16b170208f85516bc4f254e8af
linnnruoo <linruo2014@gmail.com>
2019-01-19 21:20:20 +0800

    try drawer navigator

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation'
-import Main from './components/Main'
+// import React from 'react';
+// import { createStackNavigator, createAppContainer } from 'react-navigation'
+// import Main from './components/Main'
 
-export default class App extends React.Component {
+// export default class App extends React.Component {
   
-  render() {
-    return (
-      <AppContainer/>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <AppContainer/>
+//     );
+//   }
+// }
 
-const AppNavigator = createStackNavigator({
+// const AppNavigator = createStackNavigator({
+//   Main: {
+//     screen: Main
+//   }
+// })
+
+// const AppContainer = createAppContainer(AppNavigator)
+
+import { DrawerNavigator, DrawerItems } from 'react-navigation';
+import Main from './components/Main'
+import Signup from './components/Signup'
+import { Text } from 'react-native';
+
+const App = DrawerNavigator({
   Main: {
     screen: Main
+  },
+  Signup: {
+    screen: Signup
   }
-})
+}, {
+  contentComponent: (props) => (
+    <View>
+      <Text>GarangFoodies</Text>
+      <DrawerItems {...props} />
+    </View>
+  )
+});
 
-const AppContainer = createAppContainer(AppNavigator)
+export default App;
