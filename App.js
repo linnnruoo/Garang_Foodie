34d15bb9c97053a99573e96371ccabe7e3a3cb3a
linnnruoo <linruo2014@gmail.com>
2019-01-19 22:03:30 +0800

    add button

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
-// import React from 'react';
-// import { createStackNavigator, createAppContainer } from 'react-navigation'
-// import Main from './components/Main'
-
-// export default class App extends React.Component {
-  
-//   render() {
-//     return (
-//       <AppContainer/>
-//     );
-//   }
-// }
-
-// const AppNavigator = createStackNavigator({
-//   Main: {
-//     screen: Main
-//   }
-// })
-
-// const AppContainer = createAppContainer(AppNavigator)
-
-import { DrawerNavigator, DrawerItems } from 'react-navigation';
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 import Main from './components/Main'
-import Signup from './components/Signup'
-import { Text } from 'react-native';
 
-const App = DrawerNavigator({
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      <AppContainer/>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
   Main: {
     screen: Main
-  },
-  Signup: {
-    screen: Signup
   }
-}, {
-  contentComponent: (props) => (
-    <View>
-      <Text>GarangFoodies</Text>
-      <DrawerItems {...props} />
-    </View>
-  )
-});
+})
 
-export default App;
+const AppContainer = createAppContainer(AppNavigator)
