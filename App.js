95b241f3d0f2f92187086203ba3f68122721eb0c
linnnruoo <linruo2014@gmail.com>
2019-01-19 21:03:47 +0800

    clean route on App

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import Main from './components/Main'
-import Signup from './components/Signup'
-import Login from './components/Login'
 
 export default class App extends React.Component {
   
@@ -16,12 +14,6 @@ export default class App extends React.Component {
 const AppNavigator = createStackNavigator({
   Main: {
     screen: Main
-  },
-  Signup: {
-    screen: Signup
-  },
-  Login: {
-    screen: Login
   }
 })
 
