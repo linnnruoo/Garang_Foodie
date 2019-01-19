364fdac669678f43aefa8aedd16a8a263b025f97
linnnruoo <linruo2014@gmail.com>
2019-01-19 17:23:59 +0800

    able to sign up with email and password

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import fire from '../fire';
+import uuid from 'react-native-uuid'
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    }
+    this._onChangeText = this._onChangeText.bind(this);
+    this._loginUser = this._loginUser.bind(this);
+  }
+
+  _onChangeText = (input_field) => (text) => {
+    this.setState({ [input_field]: text });
+  }
+
+  _loginUser = () => {
+    fire.database().ref('users/').push({
+      id: id,
+      email: this.state.email,
+      password: this.state.password,
+      name: this.state.name
+    }, () => {
+      Alert.alert('Registered!');
+      console.log("test")
+    })
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <Text>HELOOW</Text>
+        <Text>Login</Text>
+        <TextInput
+          value={this.state.email}
+          onChangeText={this._onChangeText('email')}
+          placeholder="Email"
+        />
+        <TextInput
+          secureTextEntry
+          value={this.state.password}
+          onChangeText={this._onChangeText('password')}
+          placeholder="Password"
+        />
+        <Button title="Login" onPress={this._loginUser} />
       </View>
     )
   }
--- components/signup/Signup.js
+++ components/signup/Signup.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import fire from '../fire';
+import uuid from 'react-native-uuid'
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    }
+    this._onChangeText = this._onChangeText.bind(this);
+    this._signupUser = this._signupUser.bind(this);
+  }
+
+  _onChangeText = (input_field) => (text) => {
+    this.setState({ [input_field]: text });
+  }
+
+  _signupUser = () => {
+    // const id = uuid.v1();
+    // fire.database().ref('users/').push({
+    //   id: id,
+    //   email: this.state.email,
+    //   password: this.state.password,
+    //   name: this.state.name
+    // }, () => {
+    //   Alert.alert('Registered!');
+    //   console.log("test")
+    // })
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(
+        this.state.email,
+        this.state.password
+      )
+      .then((res) => {
+        console.log("res!", res);
+        Alert.alert("Registered successfully!");
+      })
+      .catch((err) => {
+        console.log("error!", err);
+      })
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <Text>HELOOW</Text>
+        <Text>SIGN UP</Text>
+        <TextInput
+          value={this.state.email}
+          onChangeText={this._onChangeText('email')}
+          placeholder="Enter your email here"
+        />
+        <TextInput
+          secureTextEntry
+          value={this.state.password}
+          onChangeText={this._onChangeText('password')}
+          placeholder="Enter your password here"
+        />
+        <TextInput
+          value={this.state.name}
+          onChangeText={this._onChangeText('name')}
+          placeholder="Enter your full name here"
+        />
+        <Button title="Sign Up" onPress={this._signupUser} />
       </View>
     )
   }
