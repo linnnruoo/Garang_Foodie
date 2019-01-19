73720d8bf4484ebd56ab9873b3cbc6905af5a3b6
linnnruoo <linruo2014@gmail.com>
2019-01-19 19:52:11 +0800

    solve merge conflict successfully

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import Main from './components/Main'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 export default class App extends React.Component {
   
@@ -14,8 +16,13 @@ export default class App extends React.Component {
 const AppNavigator = createStackNavigator({
   Main: {
     screen: Main
+  },
+  Signup: {
+    screen: Signup
+  },
+  Login: {
+    screen: Login
   }
 })
 
 const AppContainer = createAppContainer(AppNavigator)
-
---
+++ components/Profile.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import fire from './fire';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    }
+    this._onChangeText = this._onChangeText.bind(this);
+    this._updateProfile = this._updateProfile.bind(this);
+  }
+
+  _onChangeText = (input_field) => (text) => {
+    this.setState({ [input_field]: text });
+  }
+
+  _updateProfile = () => {
+    // const id = uuid.v1();
+    // fire.database().ref('users/').push({
+    //   id: id,
+    //   email: this.state.email,
+    //   password: this.state.password,
+    //   name: this.state.name
+    // }, () => {
+    //   Alert.alert('Registered!');
+    //   console.log("test")
+    // })
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>Complete Your Profile</Text>
+        <TextInput
+          value={this.state.name}
+          onChangeText={this._onChangeText('name')}
+          placeholder="Enter your full name here"
+        />
+        <Button title="Update" onPress={this._updateProfile} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Profile;
