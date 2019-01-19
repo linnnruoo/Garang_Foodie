ea79980e9e77c44ab79fa3b9f12b4b7ffef5960e
linnnruoo <linruo2014@gmail.com>
2019-01-20 00:22:20 +0800

    modify route on mylist container

--- components/MyListContainer.js
+++ components/MyListContainer.js
@@ -1,54 +1,24 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import { Icon, Container, Header, Button, Right, Content, Text } from 'native-base'
-import EntryTile from './EntryTile'
-import { AppLoading } from 'expo'
-import CVTest from './CVTest';
-
+import MyList from './MyList'
+import CVTest from './CVTest'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-export default class MyList extends React.Component {
-  constructor() {
-    super()
-    this.state = { 
-      loading: true 
-    }
-  }
+import {Icon, Button} from 'native-base'
 
+export default class MyListContainer extends React.Component {
   static navigationOptions = {
     tabBarIcon: ({ tintColor }) => (
       <Icon type="FontAwesome" name="spoon" style={{ color: tintColor }} />
     )
   }
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-    })
-    this.setState({ loading: false })
-  }
-
   render() {
-    const { navigation } = this.props;
-
-    if (this.state.loading) {
-      return <AppLoading />
-    } else {
-      return (
-      <Container>
-        <Content>
-          <EntryTile />
-          <EntryTile />
-          <EntryTile />
-        </Content>
-        <TouchableOpacity onPress={() => navigation.navigate('CVTest') } style={styles.addButton}>
-          <Icon name="add" style={styles.icon} />
-        </TouchableOpacity>
-      </Container>
+    return (
+      <>
+        <ChildContainer />
+      </>
     );
   }
-}
 }
 
 const ChildNavigator = createStackNavigator({
@@ -58,26 +28,8 @@ const ChildNavigator = createStackNavigator({
   CVTest: {
     screen: CVTest
   }
+}, {
+  headerMode: 'none',
 })
 
-const ChildNavigator = createAppContainer(ChildNavigator)
-
-
-const styles = StyleSheet.create({
-  addButton: {
-    position: 'absolute',
-    zIndex: 11,
-    right: 20,
-    bottom: 20,
-    backgroundColor: '#e91e62',
-    width: 80,
-    height: 80,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 8
-  },
-  icon: {
-    color: '#fff',
-  }
-});
+const ChildContainer = createAppContainer(ChildNavigator)
